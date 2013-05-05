c9187d292565b24d80e94343619c1238a0885210
Igor Loban <igorloban92@gmail.com>
2013-05-05 22:29:51 +0300

    reduce complex of parse method

--- src/by/bsu/fpmi/cnfp/io/PlainTextInputData.java
+++ src/by/bsu/fpmi/cnfp/io/PlainTextInputData.java
@@ -35,88 +35,87 @@ public class PlainTextInputData implements InputData {
     public Net parse() {
         NetBuilder builder = new NetBuilder();
         Scanner scanner = new Scanner(inputStream);
-
-        int nodeCount;
-        int arcCount;
         Map<Integer, Node> nodes = new HashMap<>();
         Map<Integer, Arc> arcs = new HashMap<>();
 
         // Header
-        nodeCount = scanner.nextInt();
-        arcCount = scanner.nextInt();
+        int nodeCount = scanner.nextInt();
+        int arcCount = scanner.nextInt();
         builder.setNodeCount(nodeCount).setArcCount(arcCount).setEps(scanner.nextDouble());
 
         // Body
         int time = 0;
         while (scanner.hasNext()) {
-            // Prepare to parse net #time
             int nodeBase = time * nodeCount;
             int arcBase = time > 0 ? (time - 1) * (arcCount + nodeCount) + arcCount : 0;
-            int nodeNumber = nodeBase;
-            int arcNumber = arcBase + 1;
-            while (nodeNumber < nodeBase + nodeCount) {
-                Node node = new Node(++nodeNumber);
-                nodes.put(nodeNumber, node);
-                builder.addNode(node);
+            createNodes(nodes, builder, nodeBase, nodeCount);
+            createArcs(arcs, builder, arcBase, time > 0 ? arcCount + nodeCount : arcCount);
+
+            for (int nodeNumber = nodeBase + 1; nodeNumber <= nodeBase + nodeCount; nodeNumber++) {
+                scanner.next();
+                parseNode(nodes.get(nodeNumber), scanner);
             }
+
             if (time > 0) {
-                while (arcNumber <= arcBase + nodeCount) {
-                    Arc arc = new Arc(arcNumber);
-                    arcs.put(arcNumber++, arc);
-                    builder.addArc(arc);
+                int arcNumber = arcBase;
+                int nodeNumber = nodeBase;
+                for (int i = 0; i < nodeCount; i++) {
+                    initIntermediateArc(arcs.get(++arcNumber), nodes.get(++nodeNumber),
+                            nodes.get(nodeNumber - nodeCount - 1));
                 }
             }
+
+            int arcNumber = time > 0 ? arcBase + nodeCount : arcBase;
             for (int i = 0; i < arcCount; i++) {
-                Arc arc = new Arc(arcNumber);
-                arcs.put(arcNumber++, arc);
-                builder.addArc(arc);
+                scanner.next();
+                parseArc(arcs.get(++arcNumber), nodes, nodeBase, scanner);
             }
 
-            // Parse net #time
-            String mark = scanner.next();
-
-            // Parse Node
-            nodeNumber = nodeBase;
-            while ("n".equals(mark)) {
-                Node node = nodes.get(++nodeNumber);
-                node.setProductivity(scanner.nextInt());
-                node.setCapacity(scanner.nextInt());
-                node.setCost(scanner.nextInt());
-                mark = scanner.next();
-            }
-
-            // Build intermediate arcs
-            arcNumber = arcBase + 1;
-            if (time > 0) {
-                for (int i = 1; i <= nodeCount; i++) {
-                    Arc arc = arcs.get(arcNumber++);
-                    Node beginNode = nodes.get(nodeBase - nodeCount + i);
-                    arc.setBeginNode(beginNode);
-                    arc.setEndNode(nodes.get(nodeBase + i));
-                    arc.setCapacity(beginNode.getCapacity());
-                    arc.setCost(beginNode.getCost());
-                }
-            }
-
-            // Parse Arc
-            while ("a".equals(mark)) {
-                Arc arc = arcs.get(arcNumber++);
-                arc.setBeginNode(nodes.get(nodeBase + scanner.nextInt()));
-                arc.setEndNode(nodes.get(nodeBase + scanner.nextInt()));
-                arc.setCapacity(scanner.nextDouble());
-                arc.setCost(scanner.nextDouble());
-                if (scanner.hasNext()) {
-                    mark = scanner.next();
-                } else {
-                    break;
-                }
-            }
-
-            // Prepare to parse next net
             time++;
+            if (scanner.hasNext()) {
+                scanner.next();
+            }
         }
 
         scanner.close();
         return builder.build();
+    }
+
+    private void createNodes(Map<Integer, Node> nodes, NetBuilder builder, int nodeBase, int nodeCount) {
+        int nodeNumber = nodeBase;
+        while (nodeNumber < nodeBase + nodeCount) {
+            Node node = new Node(++nodeNumber);
+            nodes.put(nodeNumber, node);
+            builder.addNode(node);
+        }
+    }
+
+    private void createArcs(Map<Integer, Arc> arcs, NetBuilder builder, int arcBase, int arcCount) {
+        int arcNumber = arcBase;
+        while (arcNumber < arcBase + arcCount) {
+            Arc arc = new Arc(++arcNumber);
+            arcs.put(arcNumber, arc);
+            builder.addArc(arc);
+        }
+    }
+
+    private void parseNode(Node node, Scanner scanner) {
+        node.setProductivity(scanner.nextInt());
+        node.setCapacity(scanner.nextInt());
+        node.setCost(scanner.nextInt());
+    }
+
+    private void parseArc(Arc arc, Map<Integer, Node> nodes, int nodeBase, Scanner scanner) {
+        arc.setBeginNode(nodes.get(nodeBase + scanner.nextInt()));
+        arc.setEndNode(nodes.get(nodeBase + scanner.nextInt()));
+        arc.setCapacity(scanner.nextDouble());
+        arc.setCost(scanner.nextDouble());
+    }
+
+    private void initIntermediateArc(Arc arc, Node beginNode, Node endNode) {
+        arc.setBeginNode(beginNode);
+        arc.setEndNode(endNode);
+        arc.setCapacity(beginNode.getCapacity());
+        arc.setCost(beginNode.getCost());
     }
 }
